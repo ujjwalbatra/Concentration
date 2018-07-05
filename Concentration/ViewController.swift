557bbd0c4165d3f720fbafc480f2f93ef6cd598d
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2018-07-05 12:36:28 +0530

    first commit.

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet weak var flipCountLabel: UILabel!
+    var emojiChoices = ["🎃","🎃","👻","👻"]
+    
+    var flipCount = 0{
+        didSet {//this is called property observer. this is going to be changed everytime value of the property changes
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        let cardNumber = cardButtons.index(of: sender)!
+        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
     }
-
-
+    
+ 
+    func flipCard( withEmoji emoji: String, on button: UIButton) {
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
 
